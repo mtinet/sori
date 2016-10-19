ded27a7f7cf4861903a630c396ef3978a8d65892
Jason Juhyun Kim <mtinet@hanmail.net>
2016-10-19 13:45:35 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var song;
+
+function preload() {
+  song = loadSound('assets/Two Door Cinema Club -Handshake (Amtrac Remix).mp3');
+}
+
+function setup() {
+  createCanvas(710, 200);
+  song.loop(); // song is ready to play during setup() because it was loaded during preload
+  background(0,255,0);
+}
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.pause(); // .play() will resume from .pause() position
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
+}
